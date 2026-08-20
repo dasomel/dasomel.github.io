aea95c5f1cf097ce3d1cf69dbad412c9437735c8
dasomel <dasomell@gmail.com>
2026-08-20 09:52:35 +0900

    seo: use production domain in locale metadata

--- app/[locale]/layout.tsx
+++ app/[locale]/layout.tsx
@@ -7,7 +7,7 @@ import Header from '@/components/layout/Header';
 import Footer from '@/components/layout/Footer';
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://dasomel.github.io'),
+  metadataBase: new URL('https://cne.io.kr'),
 };
 
 export function generateStaticParams() {
